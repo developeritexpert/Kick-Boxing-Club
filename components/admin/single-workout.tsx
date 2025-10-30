49b2ecd4363998c731037e7e6b5cc8d052a0e697
developeritexpert <developeritexpert3@gmail.com>
2025-10-30 13:38:01 +0530

    view: workout library page

--- components/admin/single-workout.tsx
+++ components/admin/single-workout.tsx
@@ -14,15 +14,24 @@ const SingleWorkout: React.FC = () => {
                     className="workout-image"
                 />
                 {/* <Image src="https://picsum.photos/800/400?random=1" alt="Workout" width={800} height={400} className="workout-image" priority /> */}
+                <div className="casting-cnt">
+                     <h1 className="workout-title">Morning Kickboxing Live</h1>
+                     <img src="/casting_icon.png" alt="casting img"/>
 
-                <h1 className="workout-title">Morning Kickboxing Live</h1>
+                </div>
 
                 <div className="workout-meta">
                     <div className="meta-item">
+                          <span className="meta-icon">
+                            <img src="/duration_icon.png" alt="tick_icon"/>
+                          </span>
                         <span className="meta-label">Duration:</span>
                         <span className="meta-value">60 min</span>
                     </div>
                     <div className="meta-item">
+                         <span className="meta-icon">
+                            <img src="/tick_icon.png" alt="duration_icon"/>
+                          </span>
                         <span className="meta-label">Status:</span>
                         <span className="meta-value status-active">Active</span>
                     </div>
